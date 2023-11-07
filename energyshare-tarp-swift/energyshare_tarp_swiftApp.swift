8dd2b443693f8a74a4aa6c6bc6621c67c3a97d37
Ananya Elizabeth George <50709725+ananya190@users.noreply.github.com>
2023-11-07 12:36:09 +0530

    Initial Commit

---
+++ energyshare-tarp-swift/energyshare_tarp_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  energyshare_tarp_swiftApp.swift
+//  energyshare-tarp-swift
+//
+//  Created by Ananya George on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct energyshare_tarp_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
